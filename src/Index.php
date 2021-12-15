8ae68a1d13f0c27b490822872697950466ff871b
luca <654115233@qq.com>
2021-12-15 09:35:30 +0800

    add

---
+++ src/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace Test;
+
+class Index
+{
+
+    public function index()
+    {
+        echo "this is IndexController index function";
+    }
+}
---
+++ src/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Test;
+
+
+class Test
+{
+
+    public function test()
+    {
+        echo "test";
+    }
+}
